989634bd5c52b74b369483e1915e9afd56546cea
ahmed-razic <ahmed.razic@gmail.com>
2022-04-03 20:22:58 +0200

    added local storage

--- appes6.js
+++ appes6.js
@@ -46,6 +46,50 @@ class UI {
   }
 }
 
+class Store {
+  static getBooks() {
+    let books;
+    if (localStorage.getItem('books') === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem('books'));
+    }
+
+    return books;
+  }
+
+  static displayBooks() {
+    const books = Store.getBooks();
+
+    books.forEach(function (book) {
+      const ui = new UI();
+      ui.addBook(book);
+    });
+  }
+
+  static addBook(book) {
+    const books = Store.getBooks();
+    books.push(book);
+
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+
+  static removeBook(isbn) {
+    const books = Store.getBooks();
+    books.forEach(function (book, index) {
+      if (book.isbn === isbn) {
+        books.splice(index, 1);
+      }
+    });
+
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+}
+
+//DOM LOADED EVENT
+
+document.addEventListener('DOMContentLoaded', Store.displayBooks);
+
 //Event listener to add a book
 document.querySelector('#book-form').addEventListener('submit', function (e) {
   e.preventDefault();
@@ -62,6 +106,8 @@ document.querySelector('#book-form').addEventListener('submit', function (e) {
     ui.showAlert('Please fill in all fields', 'error');
   } else {
     ui.addBook(book);
+
+    Store.addBook(book);
     ui.showAlert('Book added!', 'success');
     ui.clearBooks();
   }
@@ -73,6 +119,8 @@ document.getElementById('book-list').addEventListener('click', function (e) {
 
   const ui = new UI();
   ui.deleteBook(e.target);
+
+  Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
 
   ui.showAlert('Book deleted', 'success');
 });
